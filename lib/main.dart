456259e94110b5b51acc10162bf14146cc529083
최경선 / 서비스개발담당 WEB개발팀 <156506@ssg.com>
2023-02-14 08:43:41 +0900

    feat: learning widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            // mainAxisSize: MainAxisSize.min,
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            // verticalDirection: VerticalDirection.up,
+            children: [
+              Container(
+                // width: double.infinity,
+                height: 100.0,
+                color: Colors.white,
+                child: Text('Container 1'),
+              ),
+              SizedBox(height: 20.0),
+              Container(
+                // width: double.infinity,
+                height: 100.0,
+                color: Colors.blue,
+                child: Text('Container 2'),
+              ),
+              Container(
+                // width: double.infinity,
+                height: 100.0,
+                color: Colors.red,
+                child: Text('Container 3'),
+              ),
+              // Container(
+              //   width: double.infinity,
+              //   height: 10.0,
+              // ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
