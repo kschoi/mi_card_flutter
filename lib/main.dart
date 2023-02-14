19e7bcdf7a120ed9fba801dcd1f72f9808bda458
최경선 / 서비스개발담당 WEB개발팀 <156506@ssg.com>
2023-02-14 10:03:12 +0900

    feat: complete mi card

--- lib/main.dart
+++ lib/main.dart
@@ -9,39 +9,112 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.deepPurple,
         body: SafeArea(
           child: Column(
-            // mainAxisSize: MainAxisSize.min,
-            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            // verticalDirection: VerticalDirection.up,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                // width: double.infinity,
-                height: 100.0,
-                color: Colors.white,
-                child: Text('Container 1'),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/profile.png'),
               ),
-              SizedBox(height: 20.0),
-              Container(
-                // width: double.infinity,
-                height: 100.0,
-                color: Colors.blue,
-                child: Text('Container 2'),
+              Text(
+                'Chloe',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
-              Container(
-                // width: double.infinity,
-                height: 100.0,
-                color: Colors.red,
-                child: Text('Container 3'),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.deepPurple.shade100,
+                  letterSpacing: 2.5,
+                ),
               ),
-              // Container(
-              //   width: double.infinity,
-              //   height: 10.0,
-              // ),
+              SizedBox(
+                width: 150.0,
+                height: 20.0,
+                child: Divider(color: Colors.deepPurple.shade100),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.deepPurple,
+                  ),
+                  title: Text(
+                    '+82 010 1234 5678',
+                    style: TextStyle(
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.deepPurple.shade900,
+                      letterSpacing: 2.5,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.deepPurple,
+                  ),
+                  title: Text(
+                    'kschoi0308@gmail.com',
+                    style: TextStyle(
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.deepPurple.shade900,
+                      letterSpacing: 2.5,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
+
+          // [PRACTICE 1]
+          // child: Column(
+          //   // mainAxisSize: MainAxisSize.min,
+          //   // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //   crossAxisAlignment: CrossAxisAlignment.stretch,
+          //   // verticalDirection: VerticalDirection.up,
+          //   children: [
+          //     Container(
+          //       // width: double.infinity,
+          //       height: 100.0,
+          //       color: Colors.white,
+          //       child: Text('Container 1'),
+          //     ),
+          //     SizedBox(height: 20.0),
+          //     Container(
+          //       // width: double.infinity,
+          //       height: 100.0,
+          //       color: Colors.blue,
+          //       child: Text('Container 2'),
+          //     ),
+          //     Container(
+          //       // width: double.infinity,
+          //       height: 100.0,
+          //       color: Colors.red,
+          //       child: Text('Container 3'),
+          //     ),
+          //     // Container(
+          //     //   width: double.infinity,
+          //     //   height: 10.0,
+          //     // ),
+          //   ],
+          // ),
         ),
       ),
       debugShowCheckedModeBanner: false,
